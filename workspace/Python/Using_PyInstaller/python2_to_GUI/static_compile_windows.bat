a48c39febd290e76628bae8ec8b0802d3e661b25
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2019-11-20 23:39:49 +0800

    PyInstaller updates
    PyInstaller updates for python 2 and 3

--- workspace/Python/Using_PyInstaller/python2_to_GUI/static_compile_windows.bat
+++ workspace/Python/Using_PyInstaller/python2_to_GUI/static_compile_windows.bat
@@ -2,22 +2,26 @@
 SET countdown=5
 Set "RootDir=%~d0"
 SET cwd=%cd%
-SET out_name=py2Tk
+rem SET out_name=
+rem SET src_name=main.py
 SET icon_name=res/prog_icon.ico
-SET src_name=py2Tk.py
+SET src_name=%~1
+SET /p out_name=Out File Name: 
+
+SET p2=%RootDir%\CODING\Python\Python_2\
+SET p2_scripts=%p2%\Scripts\
+SET PATH=%p2%;%p2_scripts%;%PATH%
+
+PATH
 
 echo Command Run : 
 echo [ 
-echo	pyinstaller --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile "%cd%\%src_name%"
-
-
+echo	pyinstaller --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile %src_name%
 echo ]
 echo.
 echo.
 
-rem %RootDir%\\CODING\Python\Python_2\python -m pyinstaller
-
-pyinstaller_py2 --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile "%cd%\%src_name%" && (
+python_2 -m PyInstaller --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile %src_name% && (
 	echo Success!
 	rem for /L %%[variable_name] in (starting_value, increment_value, stop_value) Do [commands]
 	for /L %%a in (1,1,%countdown%) Do SLEEP 1 && echo %%a
--- workspace/Python/Using_PyInstaller/python3_to_GUI/static_compile_windows.bat
+++ workspace/Python/Using_PyInstaller/python3_to_GUI/static_compile_windows.bat
@@ -1,20 +1,27 @@
 @echo off
 SET countdown=5
+Set "RootDir=%~d0"
 SET cwd=%cd%
-SET out_name=py2Tk
+rem SET out_name=
+rem SET src_name=main.py
 SET icon_name=res/prog_icon.ico
-SET src_name=py2Tk.py
+SET src_name=%~1
+SET /p out_name=Out File Name: 
+
+SET p3=%RootDir%\CODING\Python\Python_3\Python37\
+SET p3_scripts=%p3%\Scripts\
+SET PATH=%p3%;%p3_scripts%;%PATH%
+
+PATH
 
 echo Command Run : 
 echo [ 
 echo	pyinstaller --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile %src_name%
-
-
 echo ]
 echo.
 echo.
 
-pyinstaller --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile %src_name% && (
+python_3 -m PyInstaller --name %out_name% --icon %cwd%\%icon_name% --distpath exe --workpath tmp --specpath spec --onefile %src_name% && (
 	echo Success!
 	rem for /L %%[variable_name] in (starting_value, increment_value, stop_value) Do [commands]
 	for /L %%a in (1,1,%countdown%) Do SLEEP 1 && echo %%a
